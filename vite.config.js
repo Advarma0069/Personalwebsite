634b80d44f95de62eb3dce2b7718f2f9030ee0d7
Aditya Varma <veraditya9161@gmail.com>
2026-01-18 23:32:45 +0530

    Fix vite.config.js after leftover merge conflict

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/Personalwebsite/', 
-=======
->>>>>>> 09a066b (UI and CSS updates)
 })
